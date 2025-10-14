5a0262321ccd8a94d2d9bba8e90dbee822d6beb0
Gina Seo <138332474+ginaaseo@users.noreply.github.com>
2025-10-14 17:31:30 -0400

    Update

--- preprocessing/data_processing.py
+++ preprocessing/data_processing.py
@@ -1,4 +1,34 @@
 import pyspark 
 import pandas as pd 
 import numpy as np 
-import math  
+import math   
+
+
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructField, StructType, StringType, LongType, IntegerType, FloatType
+from pyspark.sql.functions import col, column
+from pyspark.sql.functions import expr
+from pyspark.sql.functions import split
+from pyspark.sql import Row 
+
+
+
+
+ss=SparkSession.builder.master("local").appName("Spotify Playlist Generator").getOrCreate() 
+
+
+ss.sparkContext.setCheckpointDir("~/scratch") 
+
+
+
+spotify_DF = ss.read.csv("spotify_dataset.csv", header=True, inferSchema=True) 
+
+
+
+spotify_DF.printSchema() 
+
+
+spotify_DF.first() 
+
